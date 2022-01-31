0596d2779fb7650259da047dbceac64d02ec1c76
katherin <ft.katheochoa@gmail.com>
2022-01-31 17:56:54 -0500

    lists exercises

---
+++ lists.py
@@ -0,0 +1,17 @@
+
+# Define a list of names.
+
+names = ["Harry","Ron","Hermione","Ginny"]
+print(names)
+
+#["Harry", "Ron", "Hermione", "Ginny"]
+
+names.append("Draco")
+print(names)
+
+#["Harry","Ron","Hermione","Ginny","Draco"]
+
+names.sort()
+print(names)
+
+#["Draco", "Ginny", "Harry", "Hermione", "Ron"]
