cd84d0951984a96ec735eb0bf747af7e55dd77dc
katherin <ft.katheochoa@gmail.com>
2022-02-01 17:24:58 -0500

    added templaes for newyear folder

--- 02.Django/lecture3/hello/views.py
+++ 02.Django/lecture3/hello/views.py
@@ -5,7 +5,17 @@ from django.shortcuts import render
 # In order to create a view in Django, we're going to define a function.
 
 def index(request): 
-    return HttpResponse("Hello, world!");
+    #return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")
 
 def brian(request):
-    return HttpResponse("Hello, Brian!");
+    return HttpResponse("Hello, Brian!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name):
+    #return HttpResponse(f"Hello, {name.capitalize()}")
+    return render(request, "hello/greet.html",{
+        "name" : name.capitalize()
+    })
